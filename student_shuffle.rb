64ca97fa56b1cb68d5ab8a2bb1f91f946e1a6130
Jelani Hylton <Jelani.Hylton@yahoo.com>
2017-04-10 10:03:43 -0400

    added a sort to the array

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,6 +1,5 @@
 # Write a program that splits the class into teams of 2. If there's an odd number of students the last group should be a group of 3.
-def students
 students = ["Heather" , "demtri" , "Jelani" , "Sean", "Finn"]
-end
+students.shuffle!
 
-puts "#{students}"
+puts students
