76169cc07976276ac7f5dc6d70178699f0e23851
Jelani Hylton <Jelani.Hylton@yahoo.com>
2017-04-10 09:26:33 -0400

    first commit

---
+++ states.rb
@@ -0,0 +1,13 @@
+# puts "List the states you have visited"
+# user = ""
+# states_array=[]
+# until user == "done" do
+
+# user = gets.chomp.capitalize
+# states_array.push(user)
+# end
+# puts "youve been to the following states"
+# states_array.each do |state|
+# 	puts "#{state}"
+# end
+
